48c36e8b5489191b6a8b4193df2a853e0f15f998
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2020-06-14 12:54:50 +0200

    Use 7-segment-display instance instead of raw functions

--- app/index.ts
+++ app/index.ts
@@ -1,5 +1,5 @@
 import { byId } from 'fitbit-widgets/dist/document';
-import { print, resize } from 'fitbit-widgets/dist/7-segment-display';
+import segmentDisplay from 'fitbit-widgets/dist/7-segment-display';
 import digits from 'fitbit-widgets/dist/7-segment-display/digits';
 import clock from 'clock';
 import { inbox } from 'file-transfer';
@@ -10,24 +10,9 @@ import { display } from 'display';
 import { me } from 'appbit';
 import { preferences } from 'user-settings';
 
+import type { SegmentDisplay } from 'fitbit-widgets/dist/7-segment-display';
+
 const isAODEnabled = () => display.aodActive && display.on;
-
-const dateElements = ['d0', 'd1', 'd2', 'd3'].map(id => byId(id));
-const timeElements = ['h0', 'h1', 'm0', 'm1'].map(id => byId(id));
-const secondsElements = ['s0', 's1'].map(id => byId(id));
-
-timeElements.forEach(element =>
-	resize(element, {
-		height: 110,
-		width: 50,
-	}),
-);
-[...dateElements, ...secondsElements].forEach(element =>
-	resize(element, {
-		height: 50,
-		width: 25,
-	}),
-);
 
 const padZero = (n: number) => `0${n}`.slice(-2);
 const get12hour = (date: Date) => {
@@ -63,49 +48,72 @@ const dateRef: Ref = {
 	current: '',
 };
 
-const defaultSetVisibility = (element: GraphicsElement, on: boolean) => {
-	element.style.fill = on ? config.colors.on : config.colors.off;
+const updateSegment = (segment: GraphicsElement, on: boolean) => {
+	segment.style.fill = on ? config.colors.on : config.colors.off;
 };
 
-const getUpdater = (
-	elements: Element[],
-	ref: Ref,
-	setVisibility = defaultSetVisibility,
-) => () => {
-	for (let i = 0; i < elements.length; i++) {
-		print(elements[i], ref.current[i], {
-			charMap: digits,
-			setVisibility,
-		});
-	}
-};
+const showDate = (): boolean => config.showDate && !isAODEnabled();
+const showSeconds = (): boolean => config.showSeconds && !isAODEnabled();
 
-const updateSeconds = getUpdater(secondsElements, secondsRef, (element, on) => {
-	if (config.showSeconds && !isAODEnabled()) {
-		element.style.display = 'inline';
-		element.style.fill = on ? config.colors.on : config.colors.off;
-	} else {
-		element.style.display = 'none';
-	}
-});
-const updateTime = getUpdater(timeElements, timeRef);
-const updateDate = getUpdater(
-	dateElements,
-	dateRef,
-	(element: GraphicsElement, on: boolean) => {
-		if (config.showDate && !isAODEnabled()) {
-			element.style.display = 'inline';
-			element.style.fill = on ? config.colors.on : config.colors.off;
-		} else {
-			element.style.display = 'none';
-		}
-	},
+const dateDisplays = ['d0', 'd1', 'd2', 'd3'].map(id =>
+	segmentDisplay(byId(id), {
+		charMap: digits,
+		height: 50,
+		width: 25,
+		visible: showDate(),
+		value: '',
+		updateSegment,
+	}),
 );
+const timeDisplays = ['h0', 'h1', 'm0', 'm1'].map(id =>
+	segmentDisplay(byId(id), {
+		charMap: digits,
+		height: 110,
+		width: 50,
+		visible: true,
+		value: '',
+		updateSegment,
+	}),
+);
+const secondsDisplays = ['s0', 's1'].map(id =>
+	segmentDisplay(byId(id), {
+		charMap: digits,
+		height: 50,
+		width: 25,
+		value: '',
+		visible: showSeconds(),
+		updateSegment,
+	}),
+);
+
+const getUpdater = (displays: readonly SegmentDisplay[], ref: Ref) => () => {
+	for (let i = 0; i < displays.length; i++) {
+		displays[i].value = ref.current[i];
+	}
+};
+const updateSeconds = getUpdater(secondsDisplays, secondsRef);
+const updateTime = getUpdater(timeDisplays, timeRef);
+const updateDate = getUpdater(dateDisplays, dateRef);
 
 const updateClockGranularity = () => {
 	clock.granularity = isAODEnabled() ? 'minutes' : 'seconds';
 };
 updateClockGranularity();
+
+const updateVisibility = () => {
+	for (const dateDisplay of dateDisplays) {
+		dateDisplay.visible = showDate();
+	}
+
+	for (const secondsDisplay of secondsDisplays) {
+		secondsDisplay.visible = showSeconds();
+	}
+};
+
+if (display.aodAvailable && me.permissions.granted('access_aod')) {
+	display.aodAllowed = true;
+}
+
 clock.addEventListener('tick', ({ date }) => {
 	const newSeconds = getSecondsString(date);
 	if (newSeconds !== secondsRef.current) {
@@ -126,19 +134,10 @@ clock.addEventListener('tick', ({ date }) => {
 	}
 });
 
-const update = () => {
-	updateSeconds();
-	updateTime();
-	updateDate();
-};
-
-if (display.aodAvailable && me.permissions.granted('access_aod')) {
-	display.aodAllowed = true;
-	display.addEventListener('change', () => {
-		updateClockGranularity();
-		update();
-	});
-}
+display.addEventListener('change', () => {
+	updateClockGranularity();
+	updateVisibility();
+});
 
 inbox.addEventListener('newfile', () => {
 	let file: string | undefined;
@@ -148,6 +147,10 @@ inbox.addEventListener('newfile', () => {
 		config.showSeconds = newConfig.showSeconds;
 		config.showDate = newConfig.showDate;
 		config.dateFormat = newConfig.dateFormat;
-		update();
+
+		updateSeconds();
+		updateTime();
+		updateDate();
+		updateVisibility();
 	}
 });
