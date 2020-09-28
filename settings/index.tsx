a4130d44d1a17596d320787f0751c3c7e5ddfbc7
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2020-09-28 18:56:32 +0200

    Reorg settings UI
    Conditionally show the date format setting

--- settings/index.tsx
+++ settings/index.tsx
@@ -13,52 +13,53 @@ import {
 } from '../shared/settings';
 import { gettext } from 'i18n';
 
-registerSettingsPage(() => (
-	<Page>
-		<Section title={gettext('Visibility')}>
-			<Toggle
-				label={gettext('Show date')}
-				settingsKey={SETTING_KEY_SHOW_DATE}
-			/>
-			<Toggle
-				label={gettext('Show seconds')}
-				settingsKey={SETTING_KEY_SHOW_SECONDS}
-			/>
-		</Section>
-		<Section title={gettext('Format')}>
-			<Select<{ name: string; value: Config['dateFormat'] }>
-				label={gettext('Date')}
-				options={[
-					{ name: gettext('mmdd'), value: 'mmdd' },
-					{ name: gettext('ddmm'), value: 'ddmm' },
-				]}
-				settingsKey={SETTING_KEY_DATE_FORMAT}
-			/>
-		</Section>
-		<Section title={gettext('Colors')}>
-			<Text>{gettext('On')}</Text>
-			<ColorSelect
-				value={defaultConfig.colors.on}
-				colors={[
-					activeColors.white,
-					activeColors.red,
-					activeColors.green,
-					activeColors.blue,
-				].map((color) => ({ color }))}
-				settingsKey={SETTING_KEY_COLOR_ON}
-			/>
-			<Text>{gettext('Off')}</Text>
-			<ColorSelect
-				value={defaultConfig.colors.on}
-				colors={[
-					inactiveColors.none,
-					inactiveColors.gray,
-					inactiveColors.red,
-					inactiveColors.green,
-					inactiveColors.blue,
-				].map((color) => ({ color }))}
-				settingsKey={SETTING_KEY_COLOR_OFF}
-			/>
-		</Section>
-	</Page>
-));
+registerSettingsPage(
+	({ settings: { [SETTING_KEY_SHOW_DATE]: rawShowDate } }) => (
+		<Page>
+			<Section title={gettext('Date')}>
+				<Toggle label={gettext('Show')} settingsKey={SETTING_KEY_SHOW_DATE} />
+				{rawShowDate && JSON.parse(rawShowDate) && (
+					<Select<{ name: string; value: Config['dateFormat'] }>
+						label={gettext('Format')}
+						options={[
+							{ name: gettext('mmdd'), value: 'mmdd' },
+							{ name: gettext('ddmm'), value: 'ddmm' },
+						]}
+						settingsKey={SETTING_KEY_DATE_FORMAT}
+					/>
+				)}
+			</Section>
+			<Section title={gettext('Seconds')}>
+				<Toggle
+					label={gettext('Show')}
+					settingsKey={SETTING_KEY_SHOW_SECONDS}
+				/>
+			</Section>
+			<Section title={gettext('Colors')}>
+				<Text>{gettext('On')}</Text>
+				<ColorSelect
+					value={defaultConfig.colors.on}
+					colors={[
+						activeColors.white,
+						activeColors.red,
+						activeColors.green,
+						activeColors.blue,
+					].map((color) => ({ color }))}
+					settingsKey={SETTING_KEY_COLOR_ON}
+				/>
+				<Text>{gettext('Off')}</Text>
+				<ColorSelect
+					value={defaultConfig.colors.on}
+					colors={[
+						inactiveColors.none,
+						inactiveColors.gray,
+						inactiveColors.red,
+						inactiveColors.green,
+						inactiveColors.blue,
+					].map((color) => ({ color }))}
+					settingsKey={SETTING_KEY_COLOR_OFF}
+				/>
+			</Section>
+		</Page>
+	),
+);
